35ed070335d1facdeb61ae9cae7d2ab78a3baa3a
Stavros Sentonas <71716988+StavSent@users.noreply.github.com>
2023-07-11 16:10:57 +0300

    Update Configuration.cs

--- src/YesSql.Core/Configuration.cs
+++ src/YesSql.Core/Configuration.cs
@@ -12,10 +12,6 @@ namespace YesSql
     public class Configuration : IConfiguration
     {
 
-
-
-
-    
         public Configuration()
         {
             IdentifierAccessorFactory = new PropertyAccessorFactory("Id");
